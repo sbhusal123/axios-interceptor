bac5ec20d4fd085b4b00df04fc59a5b4fb65c7cf
sbhusal123 <suryabhusal11@gmail.com>
2024-09-20 03:20:03 +0545

    Interceptor request.

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -4,6 +4,9 @@ import Storage from './storage';
 
 import AuthApi from './authApi';
 
+import { jwtDecode } from 'jwt-decode';
+import dayjs from 'dayjs';
+
 
 const API_URL = "http://localhost:8000/api"
 
@@ -13,32 +16,58 @@ const api = axios.create({
 
 //interceptors: https://axios-http.com/docs/interceptors
 
-api.interceptors.request.use(config => {
-    const token = Storage.getAccessToken()
-    
-    if(token){
-        config.headers["Authorization"] = `Bearer ${token}`
+api.interceptors.request.use((config) => {
+    const accessToken = Storage.getAccessToken()
+    if(accessToken){
+        config.headers["Authorization"] = `Bearer ${accessToken}`
     }
     return config
-}, error => {
-    return Promise.reject(error)
-})
+}, error => Promise.reject(error))
 
-// api.interceptors.response.use(response => {
-//     return response
-// }, error => {
-//     const originalRequest = error.config;
-//     const isInvalidToken = error?.response?.data?.code === "token_not_valid"
-//     if(isInvalidToken ){
-//         const refreshToken = Storage.getRefreshToken()
-//         AuthApi.refreshToken(refreshToken).then((resp) => {
-//             const data = resp.data
-//             Storage.updateAccessToken(data)
-//             return api
-//         })
+// const isTokenExired = (token) => {
+//     try {
+//         const payload = jwtDecode(token)
+//         const expiryTime = payload.exp
+//         return dayjs.unix(expiryTime).diff(dayjs()) < 1
+//     } catch(err){
+//         return true
+//     }
+// }
+
+// api.interceptors.request.use(async (config) => {
+//     const accessToken = Storage.getAccessToken();
+//     const refreshToken = Storage.getRefreshToken();
+
+//     if(accessToken && refreshToken) {
+
+//         let token = ""
+
+//         try {
+//             const accessTokenExpired = isTokenExired(accessToken);
+//             const refreshTokenExpired = isTokenExired(refreshToken);
+
+//             delete config.headers["Authorization"]
+    
+//             if (!refreshTokenExpired && accessTokenExpired) {
+//                 const resp = await AuthApi.refreshToken(refreshToken);
+//                 const newAccessToken = resp.data.access;
+//                 Storage.updateAccessToken(newAccessToken);
+//                 token = newAccessToken
+//             } else if (!accessTokenExpired) {
+//                 token = accessToken
+//             }
+//             config.headers["Authorization"] = `Bearer ${accessToken}`;
+//         } catch (err) {
+//             console.error("Token error:", err);
+//             return config
+//         }
+
 //     }
 
-//     return Promise.reject(error)
-// })
+    
+//     return config;
+// }, (error) => {
+//     return Promise.reject(error);
+// });
 
 export default api;
